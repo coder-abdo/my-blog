d3b921ea9faa27077e5a84ca7ec5b4afca28b031
coder-abdo <a.h2020.2007@gmail.com>
2023-06-01 01:59:56 +0300

    first commit

---
+++ src/utils/navData.ts
@@ -0,0 +1,14 @@
+type navDataType = {
+  name: string;
+  path: string;
+};
+export const navData: navDataType[] = [
+  {
+    name: "About",
+    path: "/about/",
+  },
+  {
+    name: "Blog",
+    path: "/blog/",
+  },
+];
